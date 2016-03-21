002b0c3a4f9fd6a8962317c91cae12c7251ee173
sialvsic <sialvsic@outlook.com>
2016-03-21 09:49:43 +0800

    add answer

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function fibonacci_series(n) {
+  {
+    if (n === 1) {
+      return [0, 1];
+    } else {
+      var s = fibonacci_series(n - 1);
+      s.push(s[s.length - 1] + s[s.length - 2]);
+      return s;
+    }
 
-}
+  }
 
-module.exports = fibonacci_series;
+  module.exports = fibonacci_series;
