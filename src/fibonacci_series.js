d4f0d17ebf5cfb92d45088ba16da1e5ad3af476b
purpletianjing <TianjingSun@outlook.com>
2016-03-23 17:07:21 +0800

    Update fibonacci_series.js

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -2,15 +2,7 @@
 
 function fibonacci_series(n) {
   {
-    if (n === 1) {
-      return [0, 1];
-    } else {
-      var s = fibonacci_series(n - 1);
-      s.push(s[s.length - 1] + s[s.length - 2]);
-      return s;
-    }
 
-  }
 }
 
 module.exports = fibonacci_series;
